d86972601fbc6eb387ad812c7915fb3d992814e1
awesomearin <arinchandra5@gmail.com>
2026-03-13 19:25:38 -0500

    second time

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "p32"
+  base: '/p32/',
+  build: {
+    outDir: 'docs',
+  },
 })
